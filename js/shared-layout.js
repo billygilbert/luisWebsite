62c8012bd71ade56a5734f009b77468f5f22f30c
Billy GILBERT <billy.gilbert@gm.com>
2026-09-15 11:09:20 -0500

    Updating Bio / Performance page'

--- js/shared-layout.js
+++ js/shared-layout.js
@@ -1,29 +1,24 @@
 (function() {
+	var sharedHeader = [
+		'<div class="nametitle"><a href="index.html"><h1>Luis Alejandro Orozco</h1></a></div>',
+		'<div class="subheadertitle"><h3>Baritone / Stage Director</h3></div>',
+		'<div class="navsep1"></div>',
+		'<div class="nav1"><nav><a href="about.html">About</a></nav></div>',
+		'<div class="nav2"><nav><a href="music.html">Music</a></nav></div>',
+		'<div class="nav3"><nav><a href="photos.html">Photos</a></nav></div>',
+		'<div class="nav4"><nav><a href="videos.html">Videos</a></nav></div>',
+		'<div class="nav5"><nav><a href="performances.html">Performances</a></nav></div>',
+		'<div class="nav6"><nav><a href="directing.html">Stage Directing</a></nav></div>',
+		'<div class="nav7"><nav><a href="contact.html">Contact</a></nav></div>',
+		'<div class="navsep2"></div>'
+	].join('');
+
 	function loadSharedHeader() {
 		var headers = document.querySelectorAll('[data-shared-header]');
 
-		if (!headers.length) {
-			return;
+		for (var i = 0; i < headers.length; i++) {
+			headers[i].innerHTML = sharedHeader;
 		}
-
-		fetch('partials/header.html')
-			.then(function(response) {
-				if (!response.ok) {
-					throw new Error('Unable to load the shared header.');
-				}
-
-				return response.text();
-			})
-			.then(function(html) {
-				for (var i = 0; i < headers.length; i++) {
-					headers[i].innerHTML = html;
-				}
-			})
-			.catch(function(error) {
-				if (window.console) {
-					console.error(error);
-				}
-			});
 	}
 
 	if (document.readyState === 'loading') {
